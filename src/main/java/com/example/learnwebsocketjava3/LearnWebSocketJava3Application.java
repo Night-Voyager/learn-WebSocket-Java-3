0ed76a00df9feebc4df9cc418fb9113491fa4e93
Night-Voyager <18722007@bjtu.edu.cn>
2022-03-16 19:07:04 +0800

    feat: initial commit

---
+++ src/main/java/com/example/learnwebsocketjava3/LearnWebSocketJava3Application.java
@@ -0,0 +1,13 @@
+package com.example.learnwebsocketjava3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnWebSocketJava3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnWebSocketJava3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learnwebsocketjava3/LearnWebSocketJava3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnwebsocketjava3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnWebSocketJava3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
